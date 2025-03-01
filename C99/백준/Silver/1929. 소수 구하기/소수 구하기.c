a79980cd8ca519ed02241c8bf44adde1118d9c08
haamdong <h86503275@gmail.com>
2025-03-02 04:37:15 +0900

    [Silver III] Title: 소수 구하기, Time: 16 ms, Memory: 4900 KB -BaekjoonHub

---
+++ C99/백준/Silver/1929. 소수 구하기/소수 구하기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+	int num[1000001] = { 1,1, 0 }, m, n;
+	scanf("%d %d", &m, &n);
+
+	for (int i = 2; i <= n; i++) 
+		if (num[i] == 0) 
+			for (int j = 2; i * j <= n; j++)
+				num[i * j] = 1;
+
+	for (int i = m; i <= n; i++)
+		if (num[i] == 0) 
+			printf("%d\n", i);
+
+	return 0;
+}
